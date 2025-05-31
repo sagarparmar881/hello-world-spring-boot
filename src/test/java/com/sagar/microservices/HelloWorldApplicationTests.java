22cba489104cd2405f975cd1259a27179a59520f
Sagar Parmar <sagarparmar881@gmail.com>
2025-05-31 17:29:12 +0530

    feat :: microservices :: Initial commit.

---
+++ src/test/java/com/sagar/microservices/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
